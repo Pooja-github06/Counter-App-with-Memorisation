4e6365f22a8908bc1d04578ef4b2784be9274864
Pooja-github06 <pujashingte06@gmail.com>
2024-10-07 16:12:44 +0530

    changes added as on 07-10-2024

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./style.css";
+import CounterApp  from "./CounterApp";
 
 export default function App() {
   return (
     <div>
       <h4>Memorisation used in React app</h4>
-      <p>Start editing to see some magic happen :)</p>
+  
     </div>
   );
 }
---
+++ src/CounterApp.js
@@ -0,0 +1,14 @@
+import React,{useMemo,useState} from 'react'
+
+
+function CounterApp(){
+    
+
+    
+return(
+    <>
+    </>
+)
+}
+
+export default CounterApp;
