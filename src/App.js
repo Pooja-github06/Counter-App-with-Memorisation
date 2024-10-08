a0acfac3e3d4c5834d69db9b73048aa9487244ee
Pooja-github06 <pujashingte06@gmail.com>
2024-10-08 14:44:04 +0530

    changes by pooja as on 08-10-2024

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React, { useState } from "react";
+import React, { useState ,useMemo} from "react";
 import "./style.css";
 import CounterApp from './CounterApp'
 
 
 
 export default function App() {
-  const [count,setCount]=useState(0)
+  const [count,setCount]=useState(0);
+  const [otherstate,setOtherstate]=useState('');
+  const doublecount=useMemo(()=>{
+return count * 2;
+  },[count]);
   return (
-    <div>
-      <h4>Memorisation used in React app</h4>
+    <div className="main_container" >
+      <h4>Memoisation used in React app</h4>
+     <h4>Double count is {doublecount}</h4>
       <button onClick={()=>setCount(count+1)}>Increment</button>
+   
+      <input type='text' placeholder="Type here...." value={otherstate}onChange={(e)=>setOtherstate(e.target.value)}></input>
+    
     <CounterApp count={count}/>
     </div>
   );
+  
 }
--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,12 +1,7 @@
 import React,{useMemo,useState} from 'react'
 
 
-const expenseivecal=((number)=>{
-const calculation=((num)=>{
-return num * 2
-})
 
-})
 const CounterApp=React.memo(({count})=>{
     return (
       <>Count is {count}</>
